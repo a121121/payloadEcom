645b89a61c614d6c19d5e674c73bfa31e27afada
Abdullah Younas <younas.121.121@gmail.com>
2026-04-18 21:49:16 +0500

    Initial commit from Payload ecommerce template

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260411_190406_test from './20260411_190406_test';
+
+export const migrations = [
+  {
+    up: migration_20260411_190406_test.up,
+    down: migration_20260411_190406_test.down,
+    name: '20260411_190406_test'
+  },
+];
